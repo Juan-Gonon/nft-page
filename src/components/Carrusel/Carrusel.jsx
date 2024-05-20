a4271edb161594ce5278a595d2645e16015f94b1
Juan Gonon <juanfegonon27@gmail.com>
2024-05-20 14:23:39 -0600

    arrow swiper

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
-import { Swiper, SwiperSlide} from 'swiper/react'
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Scrollbar, A11y, EffectCards } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
@@ -12,19 +12,18 @@ import img3 from '../../assets/nft/bighead-2.svg'
 import img4 from '../../assets/nft/bighead-3.svg'
 import img5 from '../../assets/nft/bighead-4.svg'
 import img6 from '../../assets/nft/bighead-5.svg'
+import arrow from '../../assets/Arrow.svg'
 
 export const Carrusel = () => {
   return (
     <Container>
       <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={3}
+        autoplay={{delay: 200, disableOnInteraction: false}}
+        modules={[Navigation, Pagination, Scrollbar, A11y, EffectCards]}
         navigation
-        pagination={{ clickable: true }}
+        pagination={{type: 'fraction'}}
         scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
+        effect={'cards'}
       >
         <SwiperSlide> <img src={img1} alt='bighead' width={500} height={400} /> </SwiperSlide>
         <SwiperSlide> <img src={img2} alt='bighead' width={500} height={400} /> </SwiperSlide>
@@ -64,5 +63,58 @@ const Container = styled.div`
     .swiper-slide{
         background: ${({theme}) => theme.carouselColor};
         border-radius: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+        img{
+          display: block;
+          width: 100%;
+          height: auto;
+          object-fit: cover;
+        }
+
+    }
+
+    .swiper-button-next{
+      color: ${({theme}) => theme.text};
+      right: 0;
+      width: 4rem;
+      top: 60%;
+      background-image: url(${arrow});
+      background-position: center;
+      background-size: cover;
+
+      &::after{
+        display: none;
+      }
+
+      @media screen and (max-width: 64em) {
+        width: 3rem;
+      }
+      @media screen and (max-width: 30em) {
+        width: 2rem;
+      }
+    }
+    .swiper-button-prev{
+      color: ${({theme}) => theme.text};
+      right: 0;
+      width: 4rem;
+      top: 60%;
+      background-image: url(${arrow});
+      background-position: center;
+      background-size: cover;
+      transform: rotate(180deg);
+
+      &::after{
+        display: none;
+      }
+
+      @media screen and (max-width: 64em) {
+        width: 3rem;
+      }
+      @media screen and (max-width: 30em) {
+        width: 2rem;
+      }
     }
 `
