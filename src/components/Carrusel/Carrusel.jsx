429b5affe274f291b42461b92d340a3f8c5641b9
Juan Gonon <juanfegonon27@gmail.com>
2024-05-20 11:30:48 -0600

    importing swiper modules

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -1,3 +1,10 @@
+import styled from 'styled-components'
+import { Swiper, SwiperSlide} from 'swiper/react'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 
 export const Carrusel = () => {
   return (
