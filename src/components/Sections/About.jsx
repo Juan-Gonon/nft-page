6294a4c6e20e7f18b2a50d73e0645b352fde47ee
Juan Gonon <juanfegonon27@gmail.com>
2024-05-20 11:24:58 -0600

    about and i swiper

--- src/components/Sections/About.jsx
+++ src/components/Sections/About.jsx
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
+import { Carrusel } from '../Carrusel/Carrusel'
 
 export const About = () => {
   return (
-    <Section id='about' > Section About</Section>
+    <Section id='about' >
+      <Container>
+        <Box>
+          <Carrusel />
+        </Box>
+      </Container>
+    </Section>
   )
 }
 
@@ -16,4 +23,45 @@ const Section = styled.section`
     position: relative;
     overflow: hidden;
 
+`
+
+const Container = styled.div`
+  width: 75%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 70em) {
+    width: 85%;
+  }
+
+  @media screen and (max-width: 64em) {
+    width: 100%;
+    flex-direction: column;
+
+    &>*:last-child{
+      width: 80%;
+    }
+  }
+
+  @media screen and (max-width: 40em){
+    &>*:last-child{
+      width: 90%;
+    }
+  }
+`
+
+const Box = styled.div`
+  width: 50%;
+  height: 100%;
+  min-height: 60vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 40em) {
+    min-height: 50vh;
+  }
 `
