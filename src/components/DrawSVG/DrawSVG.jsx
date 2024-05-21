b023675c57be1a1c39da9c277db518326ada2430
Juan Gonon <juanfegonon27@gmail.com>
2024-05-20 22:37:35 -0600

    animation Ball

--- src/components/DrawSVG/DrawSVG.jsx
+++ src/components/DrawSVG/DrawSVG.jsx
@@ -1,7 +1,34 @@
-import styled from "styled-components"
+import styled, {keyframes} from 'styled-components'
 
 export const DrawSVG = () => {
   return (
-    <div>DrawSVG</div>
+    <>
+      <Ball />
+    </>
   )
 }
+
+const Bounce = keyframes`
+    from{
+        transform: translateX(-50%) scale(.5);
+    }
+    to{
+        transform: translateX(-50%) scale(1);
+    }
+`
+
+const Ball = styled.div`
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 1.5rem;
+    height: 1.5rem;
+    background-color: ${({theme}) => theme.text};
+    border-radius: 50%;
+    animation: ${Bounce} .5s linear infinite alternate;
+
+    @media screen and (max-width: 48em) {
+        left: 1rem;
+    }
+`
