54eb66b407d9ec1ad516d26e16093064a1ae97c9
Juan Gonon <juanfegonon27@gmail.com>
2024-05-20 23:25:03 -0600

    scrollTrigger

--- src/components/DrawSVG/DrawSVG.jsx
+++ src/components/DrawSVG/DrawSVG.jsx
@@ -1,11 +1,56 @@
 import styled, {keyframes} from 'styled-components'
 import { Vector } from '../../assets/Icons/Vector'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import { useLayoutEffect, useRef } from 'react'
 
 export const DrawSVG = () => {
+  const ref = useRef(null)
+  const ballRef = useRef(null)
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  useLayoutEffect(() => {
+    let element = ref.current
+    let svg = document.getElementsByClassName('svg-path')[0]
+
+    const length = svg.getTotalLength()
+    console.log(length)
+    // comienza a posicionar el dibujo svg
+    svg.style.strokeDasharray = length
+
+    // ocultar andes de comenzar a desplazarse
+    svg.style.strokeDashoffset = length
+
+    let recorrido = gsap.timeline({
+      scrollTrigger: {
+        trigger: element,
+        start: 'top center',
+        end: 'bottom bottom',
+
+        onUpdate: (self) => {
+          const draw = length * self.progress
+          svg.style.strokeDashoffset = length - draw
+        },
+        onToggle: (self) => {
+          if (self.isActive) {
+            ballRef.current.style.display = 'none'
+          } else {
+            ballRef.current.style.display = 'inline-block'
+          }
+        }
+      }
+    })
+
+    return () => {
+      if (recorrido) recorrido.kill()
+    }
+  }, [])
+
   return (
     <>
-      <Ball />
-      <VectorContainer>
+      <Ball ref={ballRef} />
+      <VectorContainer ref={ref} >
         <Vector />
       </VectorContainer>
     </>
@@ -50,5 +95,9 @@ const VectorContainer = styled.div`
         display: inline-block;
         width: 100%;
         height: 100%;
+    }
+
+    @media screen and (max-width: 48em) {
+        left: 1rem;
     }
 `
